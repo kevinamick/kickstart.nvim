28c5bbca6c3fa7b75dd4cca66636ddec6d73247b
Kevin Amick <kevinamick81@gmail.com>
2023-12-29 19:57:37 -0500

    :Customise and add harpoon

--- init.lua
+++ init.lua
@@ -451,11 +451,13 @@ vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
   { desc = "Open harpoon window" })
 
 vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
-
+vim.keymap.set("n", "<leader>r", function() harpoon:list():remove() end)
 vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
 vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
 vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
 vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+
+vim.wo.relativenumber = true
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
